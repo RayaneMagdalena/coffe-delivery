b3f1b92e488da56d8e609f86f15829fb85d720ed
Rayane Magdalena <121320740+RayaneMagdalena@users.noreply.github.com>
2024-05-09 21:14:21 -0300

    Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,13 +2,42 @@ import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
 display: flex;
+justify-content: space-between;
 
 img {
     width: 5rem;
     height: 2rem;
 }
 
+nav {
+    display: flex;
+    align-items: center;
+    gap: .75rem;
+}
+
+nav div  {
+display: flex;
+align-items: center;
+gap: 0.2rem;;
+padding: .5rem;
+border-radius: 6px;
+background-color: ${(props) => props.theme['purple-light']};
+color:  ${(props) => props.theme['purple-dark']}
+
+}
+
 p {
     font-size: 14px;
 }
+`
+
+export const ButtonCart = styled.button `
+background-color: ${(props) => props.theme['yellow-light']};
+display: flex;
+align-items: center;
+border: none;
+width: 2.4rem;
+border-radius: 6px;
+padding: .5rem;
+
 `
